29348afe73d956574e6b3ad17aef3dd60c6439b3
Vsevolod Zaikov <seva.zaikov@gmail.com>
2017-05-27 18:28:30 +0200

    add middleware tests

---
+++ test/middleware.spec.ts
@@ -0,0 +1,24 @@
+import { createMiddleware } from '../src';
+import { spy } from 'sinon';
+
+test('createMiddleware should pass injected params to dispatched actions', () => {
+  const params = { some: 123 };
+  const middleware = createMiddleware({ params });
+  const dispatch = () => {};
+  const getState = () => {};
+  const action: any = spy();
+  middleware({ dispatch, getState })(() => {})(action);
+
+  expect(action.calledWith({ dispatch, getState, params })).toBe(true);
+});
+
+test('createMiddleware should invoke next if not a function', () => {
+  const params = { some: 123 };
+  const middleware = createMiddleware({ params });
+  const dispatch = () => {};
+  const getState = () => {};
+  const next = spy();
+  middleware({ dispatch, getState })(next)({ type: 'FAKE' });
+
+  expect(next.calledWith({ type: 'FAKE' })).toBe(true);
+});
