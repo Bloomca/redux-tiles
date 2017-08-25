66af86cd499cca0877cfddbd425bd3b6d06aab2a
Seva Zaikov <seva.zaikov@gmail.com>
2017-08-25 11:33:56 +0200

    update tests for new functions replacement of lodash

--- test/middleware.spec.ts
+++ test/middleware.spec.ts
@@ -1,6 +1,41 @@
 import { createMiddleware } from '../src';
 import { spy } from 'sinon';
 
+test('createMiddleware should call action if it is a function', () => {
+  const params = { some: 123 };
+  const { middleware } = createMiddleware({ params });
+  const dispatch = () => {};
+  const getState = () => {};
+  const action: any = spy();
+  middleware({ dispatch, getState })(() => {})(action);
+
+  expect(action.calledOnce).toBe(true);
+});
+
+test('createMiddleware should not call next if action is a function', () => {
+  const params = { some: 123 };
+  const { middleware } = createMiddleware({ params });
+  const dispatch = () => {};
+  const getState = () => {};
+  const action = () => {};
+  const next: any = spy();
+  middleware({ dispatch, getState })(next)(action);
+
+  expect(next.notCalled).toBe(true);
+});
+
+test('createMiddleware should call next if action is not a function', () => {
+  const params = { some: 123 };
+  const { middleware } = createMiddleware({ params });
+  const dispatch = () => {};
+  const getState = () => {};
+  const action = { some: true };
+  const next: any = spy();
+  middleware({ dispatch, getState })(next)(action);
+
+  expect(next.calledOnce).toBe(true);
+});
+
 test('createMiddleware should pass injected params to dispatched actions', () => {
   const params = { some: 123 };
   const { middleware } = createMiddleware({ params });
